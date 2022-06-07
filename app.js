6d1fb2d36ba25bc0276f1aaf0d6b99317c4d3157
pyg <parkpark14@naver.com>
2022-06-07 13:44:31 +0900

    xlsx-chart example

--- app.js
+++ app.js
@@ -7,38 +7,26 @@ const XLSXChart = require('xlsx-chart');
 const xlsxChart = new XLSXChart();
 const opts = {
     file: "chart.xlsx",
-	chart: "column",
+	// chart: "column",
+    chart: "line",
 	titles: [
-		"Title 1",
-		"Title 2",
-		"Title 3"
+		"Title 1"
 	],
 	fields: [
-		"Field 1",
-		"Field 2",
-		"Field 3",
-		"Field 4"
+		"Apple",
+		"Blackberry",
+		"Strawberry",
+		"Cowberry"
 	],
 	data: {
-		"Title 1": {
-			"Field 1": 5,
-			"Field 2": 10,
-			"Field 3": 15,
-			"Field 4": 20 
-		},
-		"Title 2": {
-			"Field 1": 10,
-			"Field 2": 5,
-			"Field 3": 20,
-			"Field 4": 15
-		},
-		"Title 3": {
-			"Field 1": 20,
-			"Field 2": 15,
-			"Field 3": 10,
-			"Field 4": 5
+		"Price": {
+			"Apple": 10,
+			"Blackberry": 5,
+			"Strawberry": 15,
+			"Cowberry": 20
 		}
-	}
+	},
+	chartTitle: "Line chart"
 }
 
 app.get("/", (req, res)=>{
@@ -52,7 +40,7 @@ app.get("/excel", (req, res) => {
     xlsxChart.generate (opts, function (err, data) {
         res.set ({
           "Content-Type": "application/vnd.ms-excel",
-          "Content-Disposition": "attachment; filename=chart.xlsx",
+          "Content-Disposition": "attachment; filename=line.xlsx",
           "Content-Length": data.length
         });
         res.status (200).send (data);
